b34f98c7715cc88e6292779a83beded2db166458
Woongjo Yoo <onikss793@gmail.com>
2020-09-21 20:18:39 +0900

    hansu

--- functions/hansu.py
+++ functions/hansu.py
@@ -1,33 +1,21 @@
 def main():
     case = int(input())
-    count = 0
+    hansu = 0
 
-    for number in range(1, case + 1):
-        print(number)
-        print(is_hansu(number))
-        if is_hansu(number):
-            count += 1
+    for n in range(1, case + 1):
+        if n <= 99:
+            hansu += 1
+        else:
+            if is_hansu(n):
+                hansu += 1
 
-    print(count)
-    return count
+    print(hansu)
 
 
 def is_hansu(number):
-    units = list(map(int, str(number).split()))
-    common_difference = units[1] - units[0] if len(units) > 1 else 0
-    first_num = units[0]
+    num_list = list(map(int, str(number)))
 
-    for n, unit in enumerate(units):
-        ap = get_ap(n + 1, first_num, common_difference)
-
-        if ap != unit:
-            return False
-
-    return True
-
-
-def get_ap(n, first_num, common_diff):
-    return first_num + (n - 1) * common_diff
+    return num_list[0] - num_list[1] == num_list[1] - num_list[2]
 
 
 main()
